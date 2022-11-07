59e7151b7197896893024d52b8ae648eb221707b
meeeiiike <g00407095@atu.ie>
2022-11-07 09:48:21 +0000

    added exception handling

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -1,17 +1,21 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("please enter a number between 0-9: ");
         Scanner myscan = new Scanner(System.in);
+
+        try{
         int myNum = myscan.nextInt();
 
-        if (myNum <= 9 || myNum > 0) {
-            System.out.println("you entered " + myNum);
-        } else {
-            System.out.println("not a valid entry");
+            if (myNum <= 9 || myNum > 0) {
+                System.out.println("you entered " + myNum);
+            }
+        }catch(InputMismatchException e) {
+            System.out.println("invalid number");
         }
-
 
     }
 }
+
