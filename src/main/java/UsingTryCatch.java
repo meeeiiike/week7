607e0c47095bd1acd0c45aa58a8d4340f466f13d
meeeiiike <g00407095@atu.ie>
2022-11-07 10:23:14 +0000

    now loops until correct entry

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("please enter a number between 0-9: ");
-        Scanner myscan = new Scanner(System.in);
 
-        try{
-        int myNum = myscan.nextInt();
+        int num = 1;
 
-            if (myNum <= 9 || myNum > 0) {
-                System.out.println("you entered " + myNum);
+        while(num == 1) {
+            System.out.println("please enter a number between 0-9: ");
+            Scanner myscan = new Scanner(System.in);
+            try {
+
+                int myNum = myscan.nextInt();
+
+                if (myNum <= 9 && myNum >= 0) {
+                    System.out.println("you entered " + myNum);
+                    num = 0;
+                } else {
+                    System.out.println("invalid number");
+                }
+
+            } catch (InputMismatchException ime) {
+                System.out.println("caused exception");
             }
-        }catch(InputMismatchException e) {
-            System.out.println("invalid number");
         }
+
 
     }
 }
